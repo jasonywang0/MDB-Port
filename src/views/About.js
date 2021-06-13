07915f8ff083301b48e7ffb1e6d625a29ca1db8b
jasonywang0 <jasonywang0@gmail.com>
2021-06-12 18:42:09 -0700

    refactored about me

--- src/views/About.js
+++ src/views/About.js
@@ -11,15 +11,24 @@ import {
 	MDBIcon,
     MDBBtn,
     MDBCol,
-    MDBRow
+    MDBRow,
+    MDBListGroup, 
+    MDBListGroupItem
 } from 'mdb-react-ui-kit';
 
 
 export default function About() {
   return (
     <MDBContainer className='pt-4'>
-        <MDBRow>
-            <MDBCol md={6} className='gx-5 mb-4'>
+
+        <MDBRow className='justify-content-center'>
+            <MDBCol md={6} className='gx-5 mb-2 text-center'>
+              <h1>WHO AM I</h1>
+            </MDBCol>
+        </MDBRow>
+
+        <MDBRow className='justify-content-center'>
+            <MDBCol md={5} className='gx-5 mb-4'>
               <div className="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
                 <img src="https://mdbootstrap.com/img/new/slides/031.jpg" className="img-fluid" />
                 <a href="#!">
@@ -29,26 +38,79 @@ export default function About() {
             </MDBCol>
         </MDBRow>
 
-        <MDBRow>
+        <MDBRow className='justify-content-center'>
+              <MDBCol md={7} className='gx-5 mb-4'>
+                <h4><strong>Facilis consequatur eligendi</strong></h4>
+                <p className="text-muted">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
+                  eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
+                  sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
+                </p>
+                <p><strong>Doloremque vero ex debitis veritatis?</strong></p>
+                <p className="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod itaque voluptate
+                  nesciunt laborum incidunt. Officia, quam consectetur. Earum eligendi aliquam illum
+                  alias, unde optio accusantium soluta, iusto molestiae adipisci et?
+                </p>
 
+              </MDBCol>
         </MDBRow>
-        
-        <div className="row">
-            <div className="col-md-6 gx-5 mb-4">
-              <h4><strong>Facilis consequatur eligendi</strong></h4>
-              <p className="text-muted">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
-                eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
-                sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
-              </p>
-              <p><strong>Doloremque vero ex debitis veritatis?</strong></p>
-              <p className="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod itaque voluptate
-                nesciunt laborum incidunt. Officia, quam consectetur. Earum eligendi aliquam illum
-                alias, unde optio accusantium soluta, iusto molestiae adipisci et?
-              </p>
-            </div>
-          </div> 
+
+        <MDBRow className='justify-content-center'>
+              <MDBCol md={7} className='gx-5 mb-4'>
+              <div>
+      <MDBListGroup horizontal className='flex-fill'>
+        <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+        <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+        <MDBListGroupItem>Morbi leo risus</MDBListGroupItem>
+        <MDBListGroupItem>Porta ac consectetur ac</MDBListGroupItem>
+        <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+      </MDBListGroup>
+
+      <MDBListGroup horizontal className='flex-fill'>
+        <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+        <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+        <MDBListGroupItem>Morbi leo risus</MDBListGroupItem>
+        <MDBListGroupItem>Porta ac consectetur ac</MDBListGroupItem>
+        <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+      </MDBListGroup>
+
+      <MDBListGroup horizontal className='flex-fill'>
+        <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+        <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+        <MDBListGroupItem>Morbi leo risus</MDBListGroupItem>
+        <MDBListGroupItem>Porta ac consectetur ac</MDBListGroupItem>
+        <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+      </MDBListGroup>
+
+      <MDBListGroup horizontal className='flex-fill'>
+        <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+        <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+        <MDBListGroupItem>Morbi leo risus</MDBListGroupItem>
+        <MDBListGroupItem>Porta ac consectetur ac</MDBListGroupItem>
+        <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+      </MDBListGroup>
+
+      <MDBListGroup horizontal className='flex-fill'>
+        <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+        <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+        <MDBListGroupItem>Morbi leo risus</MDBListGroupItem>
+        <MDBListGroupItem>Porta ac consectetur ac</MDBListGroupItem>
+        <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+      </MDBListGroup>
+
+      <MDBListGroup horizontal className='flex-fill'>
+        <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+        <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+        <MDBListGroupItem>Morbi leo risus</MDBListGroupItem>
+        <MDBListGroupItem>Porta ac consectetur ac</MDBListGroupItem>
+        <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+      </MDBListGroup>
+    </div>
+
+              </MDBCol>
+        </MDBRow> 
+
     </MDBContainer>
   );
 }
