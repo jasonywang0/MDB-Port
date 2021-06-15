d30a259ff57a595ff828a0287aea7b96e750dccf
Jason Wang <jasonywang0@gmail.com>
2021-06-15 16:01:13 -0700

    implemented contact form backend

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -3,27 +3,61 @@ import {
 	MDBContainer,
     MDBRow,
     MDBCol,
-    MDBListGroup, 
-    MDBListGroupItem,
     MDBTypography,
-    MDBInputGroup,
-    MDBInputGroupText,
-    MDBInput,
-    MDBInputGroupElement,
-    MDBCard,
-    MDBCardImage,
-    MDBCardBody,
-    MDBCardTitle,
-    MDBCardText,
     MDBIcon
-
 } from 'mdb-react-ui-kit';
-import aboutMe from '../resources/aboutMe';
-import { FaBootstrap, FaGitSquare, FaReact, FaSass, FaNode } from 'react-icons/fa';
-import { SiJavascript, SiHtml5, SiCss3, SiMongodb, SiMysql, SiJquery, SiFireBase } from 'react-icons/si';
-import { VscEllipsis } from 'react-icons/vsc';
+
 
 export default function Contact() {
+
+    const [ status, setStatus ] = React.useState(null);
+    // const [name, setName ] = React.useState('');
+    // const [email, setEmail ] = React.useState('');
+    // const [subject, setSubject ] = React.useState('');
+    // const [message, setMessage ] = React.useState('');
+
+  const [formValue, setFormValue] = React.useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+    
+  const changeValue = (e) => setFormValue((prev) => ({ ...prev, [e.target.name]: e.target.value}))
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, subject, message } = formValue;
+
+    if (!name || !email || !subject || !message) return setStatus('All fields are required.');
+
+    let details = {
+      name: name.value,
+      email: email.value,
+      subject: subject.value,
+      message: message.value,
+    };
+
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    let result = await response.json();
+
+    if (result === 'ERROR') {
+        setStatus('Something went wrong. Please try again later.')
+    } else if (result === 'SENT') {
+        setStatus('SENT');
+        setFormValue({name: '', email: '', subkect: '', message: ''});
+    }
+  };
+
+
   return (
     <MDBContainer id='contact' fluid className='text-white pb-5' style={{backgroundColor: '#191919'}}>
 
@@ -51,29 +85,29 @@ export default function Contact() {
         <MDBRow className='justify-content-center'>
             
             <div class="col-sm-9 col-md-8 col-lg-6 col-xl-5">
-
+                <div>{status}</div>
                 <div id="form-message-warning" class="mb-4 w-100 text-center"></div>
                 <div id="form-message-success" class="mb-4 w-100 text-center">
                     Your message was sent, thank you!
                 </div>
 
-                <form method="POST" id="contactForm" name="contactForm" class="contactForm" novalidate="novalidate">
+                <form id="contactForm" name="contactForm" class="contactForm" novalidate="novalidate" onSubmit={handleSubmit}>
                     <div class="row p-0 m-0">
                         <div class="col-md-12 my-2">
                             <div class="form-group">
-                                <input type="text" class="form-control" name="name" id="name" placeholder="Name"/>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Name" onChange={changeValue} required/>
                             </div>
                         </div>
 
                         <div class="col-md-12 my-2">
                             <div div class="form-group">
-                                <input type="email" class="form-control" name="email" id="email" placeholder="Email"/>
+                                <input type="email" class="form-control" name="email" id="email" onChange={changeValue} placeholder="Email"/>
                             </div>
                         </div>
 
                         <div class="col-md-12 my-2">
                             <div class="form-group">
-                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"/>
+                                <input type="text" class="form-control" name="subject" id="subject" onChange={changeValue} placeholder="Subject"/>
                             </div>
                         </div>
 
@@ -87,6 +121,7 @@ export default function Contact() {
                                     rows="8" 
                                     placeholder="Message" 
                                     aria-invalid="true" 
+                                    onChange={changeValue}
                                 ></textarea>
                                 {/* <label id="message-error" class="error" for="message">Please enter a message</label> */}
                             </div>
@@ -100,6 +135,7 @@ export default function Contact() {
                         </div>
                     </div>
                 </form>
+
             </div>
         </MDBRow>
 
