72bfa026cbf2e1f58b1ae9fc4d2f9c4599eedb3e
jasonywang0 <jasonywang0@gmail.com>
2021-06-15 17:33:05 -0700

    tweaked status message styling

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -11,13 +11,14 @@ import {
 function StatusMessage({status}) {
     let message = '';
     const style = {
-        color: 'yellow'
+        color: 'yellow',
+        fontWeight: 500
     };
 
     if (status === 'SENDING') {
         message = 'Sending Message.'
     } else if (status === 'INCOMPLETE') {
-        message = 'All fields are required.'
+        message = 'All Fields are Required.'
         style.color ='red'
     } else if (status === 'SENT') {
         message = 'Message Sent!'
@@ -66,23 +67,24 @@ export default function Contact() {
       body: JSON.stringify(details),
     });
     let result = await response.json(); // possible values are 'SENT' or 'ERROR
-    if (result === 'SENT') setFormValue({name: '', email: '', subject: '', message: ''});
-    setStatus(result);
+    console.log(result.status);
+    if (result.status === 'SENT') setFormValue({name: '', email: '', subject: '', message: ''});
+    setStatus(result.status);
   };
 
 
   return (
-    <MDBContainer id='contact' fluid className='text-white pb-5' style={{backgroundColor: '#191919', scrollMarginTop: '3.5rem'}}>
+    <MDBContainer id='contact' fluid className='text-white py-5' style={{backgroundColor: '#191919'}}>
 
-        <MDBRow className='justify-content-center pt-3'>
-            <MDBCol md={6} className='gx-5 mb-2 text-center'>
+        <MDBRow className='justify-content-center pt-4'>
+            <MDBCol md={6} className='gx-5 pb-2 text-center'>
                 <MDBTypography tag='div' className='display-6 pb-3 mb-3 border-bottom tracking-in-expand' style={{fontFamily: "'Cinzel', serif", letterSpacing: '0.5rem'}}>
                     CONTACT
                 </MDBTypography>
             </MDBCol>
         </MDBRow>
 
-        <MDBRow className='justify-content-center align-items-center text-center'>
+        <MDBRow className='p-0 m-0 justify-content-center align-items-center text-center'>
                 <div className="d-flex text-center justify-content-center align-items-center">
                     <div className='d-flex text-center justify-content-center align-items-center' style={{backgroundColor: '#bc9864', borderRadius: '50%', height: '4rem', width: '4rem'}}>
                         <MDBIcon icon='envelope' far size='2x' color='black'/>
@@ -108,19 +110,19 @@ export default function Contact() {
                     <div class="row p-0 m-0">
                         <div class="col-md-12 my-2">
                             <div class="form-group">
-                                <input type="text" class="form-control" name="name" id="name" placeholder="Name*" onChange={changeValue} required/>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Name*" value={formValue.name} onChange={changeValue} required/>
                             </div>
                         </div>
 
                         <div class="col-md-12 my-2">
                             <div div class="form-group">
-                                <input type="email" class="form-control" name="email" id="email" onChange={changeValue} placeholder="Email*"/>
+                                <input type="email" class="form-control" name="email" id="email" value={formValue.email} onChange={changeValue} placeholder="Email*"/>
                             </div>
                         </div>
 
                         <div class="col-md-12 my-2">
                             <div class="form-group">
-                                <input type="text" class="form-control" name="subject" id="subject" onChange={changeValue} placeholder="Subject*"/>
+                                <input type="text" class="form-control" name="subject" id="subject" value={formValue.subject} onChange={changeValue} placeholder="Subject*"/>
                             </div>
                         </div>
 
@@ -129,8 +131,9 @@ export default function Contact() {
                                 <textarea 
                                     name="message" 
                                     class="form-control error" 
-                                    id="message" 
-                                    cols="30" 
+                                    id="message"
+                                    value={formValue.message} 
+                                    cols="25" 
                                     rows="8" 
                                     placeholder="Message*" 
                                     aria-invalid="true" 
