c0c2d932275312b1d1b6eca79473dce3089b0da5
jasonywang0 <jasonywang0@gmail.com>
2021-06-16 14:31:44 -0700

    added smoother background transition

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import ProgressiveImage from 'react-progressive-bg-image';
+import {
+	MDBContainer,
+    MDBRow,
+    MDBCol
+} from 'mdb-react-ui-kit';
+import styled from 'styled-components';
+ 
+const StyledProgressiveImage = styled(ProgressiveImage)`
+  height: 100vh;
+  background-size: cover;
+  background-position: center center;
+  overflow: hidden;
+  position: relative;
+`;
 
 export default function Home() {
   return (
-    <div
-      id="intro"
-      className='text-center bg-image'
-      style={{ backgroundImage: "url('ring.jpg')"}}
-    >
-      <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.65)' }}>
-        <div className='d-flex justify-content-center align-items-center h-100'>
-          <div className='text-white'>
-            <h1 id='greeting' className='mb-5'>Hello, World!</h1>
-            <h2 id='i-am'>Nice to meet you.</h2>
-          </div>
-        </div>
-      </div>
-    </div>
+    <MDBContainer fluid className='p-0 m-0 justify-content-center align-items-center'>
+        <MDBRow>
+            <MDBCol className='p-0 m-0 justify-content-center text-center align-items-center'>
+                <StyledProgressiveImage
+                src='ring.jpg'
+                placeholder='ring-thumb.jpg'
+                transition="all 0.3s linear"
+                />;
+                <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}></div>
+
+                <div className='text-white' style={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}>
+                    <h1 id='greeting' className='mb-5'>Hello, World!</h1>
+                    <h2 id='i-am'>Nice to meet you.</h2>
+                </div>
+            </MDBCol>
+        </MDBRow>
+    </MDBContainer>
   );
 }
