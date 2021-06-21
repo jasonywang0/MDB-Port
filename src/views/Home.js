fc9cc32d6774ada1c8fa7782a5921654948b23fd
jasonywang0 <jasonywang0@gmail.com>
2021-06-21 14:31:55 -0700

    refactored intro

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,35 +1,50 @@
 import React from 'react';
-import ProgressiveImage from 'react-progressive-bg-image';
+// import ProgressiveImage from 'react-progressive-image';
 import {
 	MDBContainer,
     MDBRow,
     MDBCol
 } from 'mdb-react-ui-kit';
-import styled from 'styled-components';
- 
-const StyledProgressiveImage = styled(ProgressiveImage)`
-  height: 100vh;
-  width: 100vw;
-  background-size: cover;
-  background-position: center center;
-  position: absolute;
-`;
+
+
+const useProgressiveImg = (lowQualitySrc, highQualitySrc) => {
+  const [src, setSrc] = React.useState(lowQualitySrc);
+  React.useEffect(() => {
+    setSrc(lowQualitySrc);
+    const img = new Image();
+    img.src = highQualitySrc;
+    img.onload = () => {
+      setSrc(highQualitySrc);
+    };
+  }, [lowQualitySrc, highQualitySrc]);
+  return [src, { blur: src === lowQualitySrc }];
+};
+
+
+const BlurredUpImage = () => {
+  const [src, { blur }] = useProgressiveImg("./purple-thumb.jpg", "./purple.jpg");
+  return (
+      <div
+        className='d-flex flex-column text-center align-items-center justify-content-center bg-image'      
+        style={{
+        height: '100vh',
+          backgroundImage: `url(${src})`,
+          objectFit: 'contain',
+        filter: blur ? "blur(20px)" : "none",
+        transition: blur ? "none" : "filter 0.3s ease-out",
+      }}>
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}></div>
+        <h1 id='greeting' className='mb-3'>Hello, World!</h1>
+        <h2 id='nice'>Nice to meet you.</h2>
+      </div>
+  );
+};
 
 export default function Home() {
   return (
     <MDBContainer id="intro" fluid className='p-0 m-0'>
-        <StyledProgressiveImage
-          src='space.jpg'
-          placeholder='space-thumb.jpg'
-          transition="all 0.3s linear"
-        />
-        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}></div>
-        <MDBRow className='p-0 m-0 h-100 text-center align-items-center'>
-            <MDBCol className='p-0 m-0'>
-                <h1 id='greeting' className='mb-3'>Hello, World!</h1>
-                <h2 id='nice'>Nice to meet you.</h2>
-            </MDBCol>
-        </MDBRow>
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}></div>
+        <BlurredUpImage />
     </MDBContainer>
   );
 }
