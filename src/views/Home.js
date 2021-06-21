e18280c8ef6e5540078bbee68c7342cbb39576c1
jasonywang0 <jasonywang0@gmail.com>
2021-06-20 17:26:20 -0700

    changed background to space

--- src/views/Home.js
+++ src/views/Home.js
@@ -19,15 +19,15 @@ export default function Home() {
   return (
     <MDBContainer id="intro" fluid className='p-0 m-0'>
         <StyledProgressiveImage
-          src='ring.jpg'
-          placeholder='ring-thumb.jpg'
+          src='space.jpg'
+          placeholder='space-thumb.jpg'
           transition="all 0.3s linear"
         />
         <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}></div>
         <MDBRow className='p-0 m-0 h-100 text-center align-items-center'>
             <MDBCol className='p-0 m-0'>
-                <h1 id='greeting' className='fs-3 mb-3'>Hello, World!</h1>
-                <h2 id='nice' className='fs-3'>Nice to meet you.</h2>
+                <h1 id='greeting' className='mb-3'>Hello, World!</h1>
+                <h2 id='nice'>Nice to meet you.</h2>
             </MDBCol>
         </MDBRow>
     </MDBContainer>
